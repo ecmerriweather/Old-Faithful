b6ccdce259c027e81d5eecf3f0f2004f31433e8a
Elliot Carter <ejcseventy@gmail.com>
2021-02-24 16:48:00 +0000

    Added modelling and results

--- episode_5_git/old_faithful_eda.R
+++ episode_5_git/old_faithful_eda.R
@@ -14,3 +14,42 @@ hist(df$waiting)
 plot(df)
 
 
+### modelling
+
+library(Rmixmod)
+# mixmodCluster fits a Gaussian mixture model
+# nbCluster is the number of mixture components. 
+mod1 <- mixmodCluster(faithful, nbCluster=1, criterion="ICL")
+summary(mod1)
+
+mod1@bestResult@criterionValue
+
+mod2 <- mixmodCluster(faithful, nbCluster=2, criterion="ICL")
+summary(mod2)
+
+mod2@bestResult@criterionValue
+
+mod3 <- mixmodCluster(faithful, nbCluster=3, criterion="ICL")
+summary(mod3)
+
+mod3@bestResult@criterionValue
+
+nos<-vector()
+values<-vector()
+for (i in 1:10){
+  print(i)
+  mod<-mixmodCluster(faithful, nbCluster=i, criterion="ICL")
+  value<-mod@bestResult@criterionValue
+  nos<-c(nos, i)
+  values<-c(values, value)
+}
+mods<-data.frame(nos, values)
+
+plot(mods)
+str(mods)
+
+plot(mod1)
+
+plot(mod2)
+
+plot(mod3)
